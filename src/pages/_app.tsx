75c3e6ab36dd6001aca9020c9e01d631bcdbffcc
ykim5470-Lewis <58319204+ykim5470-Lewis@users.noreply.github.com>
2020-02-12 15:28:04 +0900

    session test

---
+++ src/pages/_app.tsx
@@ -0,0 +1,27 @@
+import fetch from "isomorphic-fetch";
+import cookies from "next-cookies";
+
+export default function MyApp({ Component, pageProps }) {
+  return [
+    <>
+      <Component {...pageProps} />
+    </>,
+  ];
+}
+
+MyApp.getInitialProps = async appContext => {
+  let ctxObj = appContext.ctx;
+  const { SESSION } = cookies(ctxObj);
+  let pageProps = {};
+  const res = await fetch("http://dev5.kross.kr/login/v2/api/session/exists", {
+    method: "post",
+    headers: {
+      authorization: process.env.AUTHORIZATION,
+      cookie: `SESSION=${SESSION}`,
+    },
+  });
+  let json = await res.json();
+  console.log(json);
+  pageProps = json;
+  return { pageProps };
+};
